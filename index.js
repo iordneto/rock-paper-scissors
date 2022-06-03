2f9e7feeabba8c7ed7bf5a0e778c7161a119684b
Iord Neto <iordneto95@gmail.com>
2022-06-03 17:25:16 -0300

    feat: add score board and flag to show the round winner

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ const playerHand = document.querySelector(".player-hand");
 const computerHand = document.querySelector(".computer-hand");
 const imgDirectory = "/assets/images/";
 
+const tieFlag = document.querySelector(".main .tie-flag");
+const userWinnerFlag = document.querySelector(".main .player .winner-flag");
+const computerWinnerFlag = document.querySelector(
+  ".main .computer .winner-flag"
+);
+
+const gameData = {
+  score: {
+    player: 0,
+    computer: 0,
+  },
+};
+
 //listeners
 startButton.addEventListener("click", (event) => {
   event.preventDefault();
@@ -19,6 +32,8 @@ startButton.addEventListener("click", (event) => {
 playerOptions.forEach((option) => {
   option.addEventListener("click", (event) => {
     event.preventDefault();
+    restartBoard();
+
     const userChoice = getUserChoice(option.textContent);
     const computerChoice = getComputerChoice();
 
@@ -51,28 +66,68 @@ const getComputerChoice = () => {
 };
 
 const determineWinner = (userChoice, computerChoice) => {
-  let winner = "Invalid Round";
+  let winner = "";
   if (userChoice === "rock") {
-    if (computerChoice === "rock") winner = "It's a tie!";
-    if (computerChoice === "paper") winner = "Computer wins";
-    if (computerChoice === "scissors") winner = "User wins";
+    if (computerChoice === "rock") winner = "tie";
+    if (computerChoice === "paper") winner = "computer";
+    if (computerChoice === "scissors") winner = "user";
   } else if (userChoice === "paper") {
-    if (computerChoice === "rock") winner = "User wins";
-    if (computerChoice === "paper") winner = "It's a tie!";
-    if (computerChoice === "scissors") winner = "Computer wins";
+    if (computerChoice === "rock") winner = "user";
+    if (computerChoice === "paper") winner = "tie";
+    if (computerChoice === "scissors") winner = "computer";
   } else if (userChoice === "scissors") {
-    if (computerChoice === "rock") winner = "Computer wins";
-    if (computerChoice === "paper") winner = "User wins";
-    if (computerChoice === "scissors") winner = "It's a tie!";
+    if (computerChoice === "rock") winner = "computer";
+    if (computerChoice === "paper") winner = "user";
+    if (computerChoice === "scissors") winner = "tie";
   }
+
   return winner;
 };
 
+const updateScoreBoard = (winner) => {
+  const playerScore = document.querySelector(".score .player-score p");
+  const computerScore = document.querySelector(".score .computer-score p");
+
+  if (winner && winner === "user") {
+    gameData.score.player++;
+    playerScore.textContent = gameData.score.player;
+  } else if (winner && winner === "computer") {
+    gameData.score.computer++;
+    computerScore.textContent = gameData.score.computer;
+  }
+};
+
 const showWinner = (winner) => {
-  alert(winner);
+  if (winner === "user") {
+    if (userWinnerFlag.classList.contains("hide"))
+      userWinnerFlag.classList.remove("hide");
+  } else {
+    if (winner === "computer") {
+      if (computerWinnerFlag.classList.contains("hide"))
+        computerWinnerFlag.classList.remove("hide");
+    } else {
+      tieFlag.classList.remove("hide");
+
+      if (userWinnerFlag.classList.contains("hide"))
+        userWinnerFlag.classList.add("hide");
+      if (computerWinnerFlag.classList.contains("hide"))
+        computerWinnerFlag.classList.add("hide");
+    }
+  }
+};
+
+const restartBoard = () => {
+  if (!userWinnerFlag.classList.contains("hide"))
+    userWinnerFlag.classList.add("hide");
+
+  if (!computerWinnerFlag.classList.contains("hide"))
+    computerWinnerFlag.classList.add("hide");
+
+  if (!tieFlag.classList.contains("hide")) tieFlag.classList.add("hide");
 };
 
 const playRound = (userChoice, computerChoice) => {
   const winner = determineWinner(userChoice, computerChoice);
+  updateScoreBoard(winner);
   showWinner(winner);
 };
